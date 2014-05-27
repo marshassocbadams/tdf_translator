38c508e7b62fa265a404f199e369ae898d2014b4
Bob Adams <badams@marshassoc.com>
2014-05-27 15:56:08 -0500

    Expand the view for more info

--- application/views/upload/form.php
+++ application/views/upload/form.php
@@ -1,3 +1,19 @@
+Data from the controller is automatically injected in the view, because the Controller is calling the view. 
+<br>
+Here's that array in raw form: 
+<?php
+echo "<pre>"; 
+print_r($data);
+echo "</pre>";
+?>
+
+<br>
+Here's one part of that array echoed on the page: <strong><?=$data['message']?></strong>
+<br>
+Here it is, manipulated: <strong><?=strtoupper($data['message'])?></strong>
+<br><br>
+
+
 <!-- IN PHP, DEFINE THE CONTROLLER/FUNCTION TO SEND THE FORM DATA -->
 <?=form_open('upload/index')?>
 
