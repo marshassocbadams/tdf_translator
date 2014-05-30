40f02fbd5d7adf547c5bfef4921904400fea0f22
Bob Adams <badams@marshassoc.com>
2014-05-30 13:04:13 -0500

    New Upload form

--- application/views/upload/form.php
+++ application/views/upload/form.php
@@ -1,23 +1,6 @@
-Data from the controller is automatically injected in the view, because the Controller is calling the view. 
-<br>
-Here's that array in raw form: 
-<?php
-echo "<pre>"; 
-print_r($data);
-echo "</pre>";
-?>
+<form method="post" action="<?=base_url('upload/do_upload')?>" enctype="multipart/form-data">
 
-<br>
-Here's one part of that array echoed on the page: <strong><?=$data['message']?></strong>
-<br>
-Here it is, manipulated: <strong><?=strtoupper($data['message'])?></strong>
-<br><br>
-
-
-<!-- IN PHP, DEFINE THE CONTROLLER/FUNCTION TO SEND THE FORM DATA -->
-<?=form_open('upload/index')?>
-
-	This is going to be an upload form. 
+	<input type="file" name="file_upload" size="20" />
 
 	<input type="submit" name="submit" value="Submit Form">
 
